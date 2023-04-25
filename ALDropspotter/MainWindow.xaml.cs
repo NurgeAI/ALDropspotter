36920e968ef8481ad3e39d870b1c592555b1136a
Belal Mohammed-Nur <Belal Mohammed-Nur@DESKTOP-RI6VNTQ>
2023-04-25 22:28:24 +0300

    Add project files.

---
+++ ALDropspotter/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ALDropspotter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Create MyImageElement
+        Image MyImageElement = new Image();
+
+        // Create a constructor
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effects = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effects = DragDropEffects.None;
+            }
+        }
+
+        private void MainWindow_Drop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files.Length > 0)
+            {
+                // File has been dropped successfully, do something with it
+                // For example, show the image in an Image element
+                BitmapImage image = new BitmapImage(new Uri(files[0]));
+                MyImageElement.Source = image;
+            }
+        }
+    }
+}
